ec4fd2b92e9be77cbc022c6189679b77b0eafbce
alexandra huallcca <km_17@outlook.com>
2020-12-17 16:46:56 -0500

    Estudiante02 Huallcca Ovalle

--- app/src/main/java/com/example/practicando_dam_cl/inscripcion.java
+++ app/src/main/java/com/example/practicando_dam_cl/inscripcion.java
@@ -59,6 +59,6 @@ public class inscripcion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inscripcion, container, false);
+        return inflater.inflate(R.layout.fragment_reglamento, container, false);
     }
 }
--- app/src/main/java/com/example/practicando_dam_cl/reglamento.java
+++ app/src/main/java/com/example/practicando_dam_cl/reglamento.java
@@ -59,6 +59,6 @@ public class reglamento extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reglamento, container, false);
+        return inflater.inflate(R.layout.fragment_inscripcion1, container, false);
     }
 }
